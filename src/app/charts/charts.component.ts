84e0f35b3057b692b115b78795e1b9663c64222b
Unknown <florian.bernou@gmail.com>
2018-11-09 17:27:14 +0100

    FAT themming

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {DataService} from "../data.service";
 import {Chart} from 'chart.js';
 
@@ -9,7 +9,8 @@ import {Chart} from 'chart.js';
 })
 export class ChartsComponent implements OnInit {
 
-  chart : Chart;
+  chart: Chart;
+  id = 'header';
 
   constructor(private _data: DataService) {
   }
@@ -46,5 +47,48 @@ export class ChartsComponent implements OnInit {
         }
       },
     });
+  }
+
+  scroll(el) {
+    this.id = el.id;
+    el.scrollIntoView({block: 'start', behavior: 'smooth'});
+  }
+
+  @HostListener("window:scroll", [])
+  onScroll(): void {
+    let header = document.getElementById('header');
+    let first = document.getElementById('first');
+    let second = document.getElementById('second');
+    let third = document.getElementById('third');
+
+    let headerBtn = document.getElementById('headerBtn');
+    let firstBtn = document.getElementById('firstBtn');
+    let secondBtn = document.getElementById('secondBtn');
+    let thirdBtn = document.getElementById('thirdBtn');
+
+    if (window.scrollY + window.innerHeight >= third.offsetTop + window.innerHeight / 2) {
+      headerBtn.classList.remove('active');
+      firstBtn.classList.remove('active');
+      secondBtn.classList.remove('active');
+      thirdBtn.classList.add('active');
+    }
+    else if (window.scrollY + window.innerHeight >= second.offsetTop + window.innerHeight / 2) {
+      headerBtn.classList.remove('active');
+      firstBtn.classList.remove('active');
+      secondBtn.classList.add('active');
+      thirdBtn.classList.remove('active');
+    }
+    else if (window.scrollY + window.innerHeight >= first.offsetTop + window.innerHeight / 2) {
+      headerBtn.classList.remove('active');
+      firstBtn.classList.add('active');
+      secondBtn.classList.remove('active');
+      thirdBtn.classList.remove('active');
+    }
+    else {
+      headerBtn.classList.add('active');
+      firstBtn.classList.remove('active');
+      secondBtn.classList.remove('active');
+      thirdBtn.classList.remove('active');
+    }
   }
 }
