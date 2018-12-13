7a72b0159cc6514d9e0c1e0bdb2b2960c2ce2836
Unknown <florian.bernou@gmail.com>
2018-12-13 17:39:36 +0100

    add graph and themming

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -14,14 +14,30 @@ export class ChartsComponent implements OnInit {
 
   firstCanvas: Chart;
   secondCanvas: Chart;
+  thirdCanvas: Chart;
+  fourthCanvas: Chart;
   id = 'header';
 
   constructor(private _data: DataService, private _draw: DrawService) {
   }
 
   ngOnInit() {
-    this.firstCanvas = this._draw.draw('firstCanvas', 'line', '');
-    this.secondCanvas = this._draw.draw('secondCanvas','pie', '');
+    this.firstCanvas = this._draw.draw('firstCanvas', 'bar', ['MERCEDES', 'LAMBORGHINI', 'ROLLS ROYCE', 'FORD', 'BENTLEY'], [1, 0.73, 0.43, 0.4, 0.38], 'bad');
+    this.secondCanvas = this._draw.draw('secondCanvas', 'bar', ['ASTON MARTIN', 'LOTUS', 'MAZDA', 'LADA', 'CHEVROLET'], [0.027, 0.03, 0.07, 0.08, 0.12], 'good');
+    this.thirdCanvas = this._draw.draw('thirdCanvas', 'bar', ['ROLLS ROYCE', 'ASTON MARTIN', 'MERCEDES', 'JAGUAR', 'LOTUS'], [342, 324, 227, 219, 214], 'bad');
+    this.fourthCanvas = this._draw.draw('fourthCanvas', 'bar', ['BMW I', 'ALFA ROMEO', 'FORD-CNG-TECHNIK', 'MERCEDES BENZ', 'LEXUS'], [0, 4, 5, 5, 48], 'good');
+  }
+
+  showInfos() {
+    let infos = document.getElementById('infos');
+    let blur = document.getElementById('blur');
+    if (infos.classList.contains('infosActive')) {
+      infos.classList.remove('infosActive');
+      blur.classList.remove('blurActive');
+    } else {
+      infos.classList.add('infosActive');
+      blur.classList.add('blurActive');
+    }
   }
 
   scroll(el) {
@@ -33,39 +49,49 @@ export class ChartsComponent implements OnInit {
     let first = document.getElementById('first');
     let second = document.getElementById('second');
     let third = document.getElementById('third');
+    let fourth = document.getElementById('fourth');
 
     let headerBtn = document.getElementById('headerBtn');
     let firstBtn = document.getElementById('firstBtn');
     let secondBtn = document.getElementById('secondBtn');
     let thirdBtn = document.getElementById('thirdBtn');
+    let fourthBtn = document.getElementById('fourthBtn');
 
     let firstTitle = document.getElementById('firstTitle');
     let secondTitle = document.getElementById('secondTitle');
     let thirdTitle = document.getElementById('thirdTitle');
+    let fourthTitle = document.getElementById('fourthTitle');
 
-    if (window.scrollY + window.innerHeight >= third.offsetTop + window.innerHeight / 2) {
+    if (window.scrollY + window.innerHeight >= fourth.offsetTop + window.innerHeight / 2) {
+      headerBtn.classList.remove('active');
+      firstBtn.classList.remove('active');
+      secondBtn.classList.remove('active');
+      thirdBtn.classList.remove('active');
+      fourthBtn.classList.add('active');
+    } else if (window.scrollY + window.innerHeight >= third.offsetTop + window.innerHeight / 2) {
       headerBtn.classList.remove('active');
       firstBtn.classList.remove('active');
       secondBtn.classList.remove('active');
       thirdBtn.classList.add('active');
-    }
-    else if (window.scrollY + window.innerHeight >= second.offsetTop + window.innerHeight / 2) {
+      fourthBtn.classList.remove('active');
+    } else if (window.scrollY + window.innerHeight >= second.offsetTop + window.innerHeight / 2) {
       headerBtn.classList.remove('active');
       firstBtn.classList.remove('active');
       secondBtn.classList.add('active');
       thirdBtn.classList.remove('active');
-    }
-    else if (window.scrollY + window.innerHeight >= first.offsetTop + window.innerHeight / 2) {
+      fourthBtn.classList.remove('active');
+    } else if (window.scrollY + window.innerHeight >= first.offsetTop + window.innerHeight / 2) {
       headerBtn.classList.remove('active');
       firstBtn.classList.add('active');
       secondBtn.classList.remove('active');
       thirdBtn.classList.remove('active');
-    }
-    else {
+      fourthBtn.classList.remove('active');
+    } else {
       headerBtn.classList.add('active');
       firstBtn.classList.remove('active');
       secondBtn.classList.remove('active');
       thirdBtn.classList.remove('active');
+      fourthBtn.classList.remove('active');
     }
   }
 }
--- src/app/draw.service.ts
+++ src/app/draw.service.ts
@@ -9,38 +9,36 @@ export class DrawService {
   constructor() {
   }
 
-  draw(idCanvas, type, data) {
+  draw(idCanvas, type, label, data, karma = 'bad') {
     let chart: Chart;
 
     switch (type) {
       case 'line':
-        chart = this.line(idCanvas, data);
+        chart = this.line(idCanvas, label, data);
         break;
       case 'pie':
-        chart = this.pie(idCanvas, data);
+        chart = this.pie(idCanvas, label, data);
+        break;
+      case 'bar':
+        chart = this.bar(idCanvas, label, data, karma);
         break;
       default :
-        chart = this.line(idCanvas, data);
+        chart = this.line(idCanvas, label, data);
         break;
     }
 
     return chart;
   }
 
-  line(idCanvas, data) {
+  line(idCanvas, label, data) {
     return new Chart(idCanvas, {
       type: 'line',
       data: {
-        labels: ['1', '2', '3', '4', '5', '6'],
+        labels: label,
         datasets: [
           {
-            data: [5, 10, 15, 20, 25, 30],
+            data: data,
             borderColor: '#3cba9f',
-            fill: false
-          },
-          {
-            data: [10, 5, 15, 5, 25, 50],
-            borderColor: '#ffcc00',
             fill: false
           },
         ]
@@ -54,14 +52,14 @@ export class DrawService {
             display: true,
             ticks: {
               beginAtZero: true,
-              max: 100
+              max: 2
             }
           }],
           yAxes: [{
             display: true,
             ticks: {
               beginAtZero: true,
-              max: 100
+              max: 2
             }
           }]
         },
@@ -69,7 +67,7 @@ export class DrawService {
     });
   }
 
-  pie(idCanvas, data) {
+  pie(idCanvas, label, data) {
     return new Chart(idCanvas, {
       type: 'pie',
       data: {
@@ -78,7 +76,32 @@ export class DrawService {
           {
             data: [5, 10, 15, 20, 25, 30],
             borderColor: '#333333',
-            backgroundColor: ['#ff6666', '#66ff66', '#6666ff', '#ffff66', '#66ffff', '#ff66ff'],
+            backgroundColor: ['#ff6666', '#ffffff'],
+            fill: false
+          },
+        ]
+      },
+    });
+  }
+
+  bar(idCanvas, label, data, karma) {
+    let backgroundColors = [];
+
+    if (karma === 'bad') {
+      backgroundColors = ['#ff2222', '#ff3333', '#ff4444', '#ff5555', '#ff6666'];
+    } else {
+      backgroundColors = ['#22ff22', '#33ff33', '#44ff44', '#55ff55', '#66ff66'];
+    }
+
+    return new Chart(idCanvas, {
+      type: 'bar',
+      data: {
+        labels: label,
+        datasets: [
+          {
+            data: data,
+            borderColor: '#aaaaaa',
+            backgroundColor: backgroundColors,
             fill: false
           },
         ]
